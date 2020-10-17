40cd8f2f5390770f69d0a908ace019de8667804e
Mannawar Hussain <dearmannawar@gmail.com>
2020-10-17 09:29:50 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 var names = require('starwars__names_by_mannawar')
 
-console.log(names)
-const randomNames = names.all()
+console.log(names.all)
+const randomNames = names.random()
 console.log(randomNames)
 
 class App extends React.Component {
@@ -17,7 +17,8 @@ class App extends React.Component {
       <div>
         <h1>starwars random names</h1>
         <ul>
-         <li>{randomNames}</li>       
+         <li>{randomNames}</li>
+       
         </ul>
         <button onClick={this.handleClick}>Refresh</button>
       </div>
